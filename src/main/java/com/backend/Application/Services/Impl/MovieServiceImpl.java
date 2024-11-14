caff2c03b4c7d9eed154ba6180ed96dbe17bbe8d
sametavci <sametavci03@gmail.com>
2024-11-14 13:45:05 +0100

    MovieServiceImpl all Methode done!

--- src/main/java/com/backend/Application/Services/Impl/MovieServiceImpl.java
+++ src/main/java/com/backend/Application/Services/Impl/MovieServiceImpl.java
@@ -1,34 +1,69 @@
 package com.backend.Application.Services.Impl;
 
 import com.backend.Adapters.dto.MovieDto;
+import com.backend.Adapters.mapper.MovieMapper;
 import com.backend.Application.Services.IMovieService;
+import com.backend.Application.Services.Models.Movie;
+import com.backend.Domain.Repository.IMovieRepository;
+import com.backend.exceptions.ResourceNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class MovieServiceImpl implements IMovieService {
+private  IMovieRepository iMovieRepository;
+
+    public MovieServiceImpl(IMovieRepository iMovieRepository) {
+        this.iMovieRepository = iMovieRepository;
+    }
 
     @Override
     public MovieDto createMovie(MovieDto movieDto) {
-        return null;
+        Movie movie = MovieMapper.mapToMovie(movieDto);
+        Movie savedMovie = iMovieRepository.save(movie);
+        return MovieMapper.mapToMovieDto(savedMovie);
     }
 
     @Override
     public MovieDto getMovie(long id) {
-        return null;
+        Movie movie = iMovieRepository
+                .findById(id)
+                .orElseThrow(()->
+                new ResourceNotFoundException("Could not find movie with that : " + id));
+        return MovieMapper.mapToMovieDto(movie);
     }
 
     @Override
     public List<MovieDto> getAllMovies() {
-        return List.of();
+        List<Movie> movies = iMovieRepository.findAll();
+        return  movies.stream()
+                .map(MovieMapper::mapToMovieDto)
+                .collect(Collectors.toList());
     }
 
     @Override
     public MovieDto updateMovie(long id, MovieDto updatedMovieDto) {
-        return null;
+        Movie movie = iMovieRepository
+                .findById(id)
+                .orElseThrow(()->
+                new RuntimeException("Could not find movie with that : " + id));
+        movie.setMovieName(updatedMovieDto.getMovieName());
+        movie.setDuration(updatedMovieDto.getDuration());
+        movie.setPrice(updatedMovieDto.getPrice());
+        movie.setGenre(updatedMovieDto.getGenre());
+        movie.setAgeRestriction(updatedMovieDto.getAgeRestriction());
+        Movie updatedMovie = iMovieRepository.save(movie);
+        return MovieMapper.mapToMovieDto(updatedMovie);
     }
 
     @Override
     public void deleteMovie(long id) {
-
+        Movie movie = iMovieRepository
+                .findById(id)
+                .orElseThrow(()->
+                        new ResourceNotFoundException("Could not find movie with that : " + id));
+        iMovieRepository.delete(movie);
     }
 }
