db7c04060e6e3bce6c309df8353b3e0f8b885f3a
sametavci <sametavci03@gmail.com>
2024-11-14 13:44:43 +0100

    MovieMapper done!

--- src/main/java/com/backend/Adapters/mapper/MovieMapper.java
+++ src/main/java/com/backend/Adapters/mapper/MovieMapper.java
@@ -1,4 +1,27 @@
 package com.backend.Adapters.mapper;
 
+import com.backend.Adapters.dto.MovieDto;
+import com.backend.Application.Services.Models.Movie;
+
 public class MovieMapper {
+    public static Movie mapToMovie(MovieDto movieDto){
+        return new Movie(
+                movieDto.getId(),
+                movieDto.getMovieName(),
+                movieDto.getDuration(),
+                movieDto.getGenre(),
+                movieDto.getPrice(),
+                movieDto.getAgeRestriction()
+        );
+    }
+    public static MovieDto mapToMovieDto(Movie movie){
+        return new MovieDto(
+                movie.getId(),
+                movie.getMovieName(),
+                movie.getDuration(),
+                movie.getGenre(),
+                movie.getPrice(),
+                movie.getAgeRestriction()
+        );
+    }
 }
