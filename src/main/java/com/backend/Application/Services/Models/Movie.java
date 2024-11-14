af2e38ebc7475156b040f72919cfe9b13f4c5a6c
Mümtaz203 <mitaserdogan@gmail.com>
2024-11-14 21:15:13 +0300

    sa

--- src/main/java/com/backend/Application/Services/Models/Movie.java
+++ src/main/java/com/backend/Application/Services/Models/Movie.java
@@ -1,6 +1,5 @@
 package com.backend.Application.Services.Models;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,21 +9,14 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
-@Table(name = "Movie")
+
 public class Movie {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private long id;
-    @Column(name = "movieName", nullable=false)
     private String movieName;
-    @Column(name = "duration")
     private int duration;
-    @Column(name="genre")
     private String genre;
-    @Column(name="price", nullable=false)
     private int price;
-    @Column(name="ageRestriction")
     private int ageRestriction;
 }
--- src/main/java/com/backend/Domain/Entities/SessionEntity.java
+++ src/main/java/com/backend/Domain/Entities/SessionEntity.java
@@ -1,4 +1,23 @@
 package com.backend.Domain.Entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "sessions")
+
+
 public class SessionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long Id;
+
+
 }
