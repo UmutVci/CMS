20461c5d81ab18d996f28faaaa259ddd32c1840b
CihanC1 <cihanlinx@gmail.com>
2024-11-15 13:03:21 -0500

    The associations among these four entities have been made.

--- src/main/java/com/backend/Domain/Entities/SeatEntity.java
+++ src/main/java/com/backend/Domain/Entities/SeatEntity.java
@@ -1,4 +1,42 @@
 package com.backend.Domain.Entities;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "seats")
 public class SeatEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false)
+    private int seatRow;
+
+    @Column(nullable = false)
+    private int seatColumn;
+
+    @Column(nullable = false)
+    private boolean isBooked;
+
+    @ManyToOne
+    @JoinColumn(name = "hall_id", nullable = false)
+    private HallEntity hall;
+
+    private String seatType;
+
+
+    public SeatEntity(int row, int column, boolean isBooked, HallEntity hall) {
+        this.seatRow = row;
+        this.seatColumn = column;
+        this.isBooked = isBooked;
+        this.hall = hall;
+    }
 }
--- src/main/java/com/backend/Domain/Entities/SessionEntity.java
+++ src/main/java/com/backend/Domain/Entities/SessionEntity.java
@@ -1,4 +1,49 @@
 package com.backend.Domain.Entities;
 
+
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "sessions")
 public class SessionEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String startTime;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
+    private MovieEntity movie;
+
+    @ManyToOne
+    @JoinColumn(name = "hall_id", nullable = false)
+    @JsonBackReference
+    private HallEntity hall;
+
+    public HallEntity getHall(){
+        return hall;
+    }
+
+    public void setHall(HallEntity hall){
+        this.hall = hall;
+    }
+
+    public MovieEntity getMovie(){
+        return movie;
+    }
+    public void setMovie(MovieEntity movie){
+        this.movie = movie;
+    }
+    public String getStartTime(){
+        return startTime;
+    }
+    public void setStartTime(String startTime){
+        this.startTime = startTime;
+    }
 }
